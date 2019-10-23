83945a0970a5e18e15bb78ceb98c8578ba15c335
Keith McClung <KMcclung@slb.com>
2019-10-23 11:28:23 +0100

    add in well mobilisation

---
+++ VTCProcessorWellPerformance.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+
+import pandas as pd
+from datetime import datetime
+
+"""
+A processor using the Pandas framework to manipulate
+Well processing information and allow it to be easily
+viewed in PowerBI
+
+"""
+pd.options.mode.chained_assignment = None  # default='warn'
+
+#"dataset" would be the var used to store data in PBI
+#remove this line in production
+dataset = pd.read_csv('WTExport20182019.csv')
+
+#Load the mobilisation spreadsheet
+mob = pd.read_csv('WTMobilisation20182019.csv')
+
+#Drop Entries with no RigName
+dataset.dropna(subset = ['RigName'], inplace=True, axis=0, how='any')
+#Drop rows with start, end, top or bottom as NULL or NAN
+dataset.dropna(subset = ['StartDate', 'EndDate','TopDepth','BottomDepth'], inplace=True, axis=0, how='any')
+
+
+def testDS():
+
+    """
+      Several functions to test the successful loading of the sample 
+      data - can de disgarded on production deployement.
+
+    """
+
+    #Test load
+    print(dataset.info())
+    #print(dataset['ProjectName'].unique().tolist())
+    #print(dataset['ProjectName'].value_counts())
+    #print(dataset['Project_Guid'].value_counts())
+    #activ = dataset['Activity'].value_counts()
+    #for a,b in activ.iteritems():
+    #    print(a,b)
+
+def defineProjects():
+
+    """
+       Drop duplicates, returning only the first records with unique
+       WellTrak Project GUID, WellName and RigName 
+
+       The purpose of this is to return a set of records that is a record
+       of every Well monitored by the Welltrak System
+
+       Returns: Pandas DataFrame
+
+    """
+
+    IDS = dataset.drop_duplicates(subset = ['Welltrak_Project_Guid','WellName','RigName'], keep ='first', inplace = False)
+
+    #Drop unused cols
+    IDS.drop(['JobName', 'FinalReportFlag','Borehole','Phase','StartDate','EndDate','Duration(Days)',
+    'Start_Day_Number','Activity','SubActivity','TopDepth','BottomDepth','TimeClassification','Planned_Flag'], axis=1,inplace=True)
+    IDS.drop(['NPTVendor', 'Global Name','NPTCategory','NPTSubCategory','Scope','Bit_Serial_Number','WE','WSS','AWSS','WSC','Skipped_Flag'], axis=1,inplace=True)
+
+    IDS.sort_values(by=['ProjectName','RigName','WellName'],inplace=True)
+
+    #Calcuate the initial well stats
+    for index, row in IDS.iterrows():
+
+        nptDays = calculateNPTStats(project=row['ProjectName'],rig=row['RigName'],well=row['WellName'])
+        stat = calculateWellStats(project=row['ProjectName'],rig=row['RigName'],well=row['WellName'])
+        mobilisationDays = calculateMobilisation(project=row['ProjectName'],rig=row['RigName'],well=row['WellName'])
+
+        print('Setting Stats for: ',row['ProjectName'],' | ',row['RigName'],' | ',row['WellName'])
+
+        if stat is None:
+            print('***NULL ROW***')
+            IDS = IDS.drop(index, axis=0)
+        else:
+            IDS.at[index,'PYmaxEndDT'] = stat['maxEndDT']
+            IDS.at[index,'PYminStartDT'] = stat['minStartDT']
+            IDS.at[index,'PYminTopDepthFT'] = stat['minTopDepth']*3.28084
+            IDS.at[index,'PYmaxBottomDepthFT'] = stat['maxBottomDepth']*3.28084
+            IDS.at[index,'PYTDorCDreachedDT'] = stat['TDorCDreachedDT']
+            IDS.at[index,'PYNPTDays'] = nptDays
+
+            #Calculate flatTime
+            flatTimeDays = calculateFlatTime(project=row['ProjectName'],rig=row['RigName'],well=row['WellName'],tddate=stat['TDorCDreachedDT'])
+            IDS.at[index,'PYFlatDays'] = flatTimeDays
+
+            #Days to Target Depth or Current Depth
+            drillDays = pd.Timedelta(pd.to_datetime(stat['TDorCDreachedDT']) - pd.to_datetime(row['SpudDate'])).total_seconds() / 86400.0
+            IDS.at[index,'PYdaysToTDorCD'] = drillDays
+
+            #Days from SPUD to Last DT Entry
+            opsDays = pd.Timedelta(pd.to_datetime(stat['maxEndDT']) - pd.to_datetime(row['SpudDate'])).total_seconds() / 86400.0
+            IDS.at[index,'PYOpsDays'] = opsDays
+ 
+            #OPT - SPUD to RR
+            OPT = pd.Timedelta(pd.to_datetime(row['Rig_Release']) - pd.to_datetime(row['SpudDate'])).total_seconds() / 86400.0
+            IDS.at[index,'PYOPTSpudToRR'] = (stat['maxBottomDepth']*3.28084) / OPT
+
+            #NPT
+            IDS.at[index,'PYNPTPercent'] = (nptDays / OPT) * 100
+
+            #Feet Per day 
+            IDS.at[index,'PYFeetPerDayToTD'] = (stat['maxBottomDepth']*3.28084) / drillDays
+            IDS.at[index,'PYFeetPerDayToRR'] = (stat['maxBottomDepth']*3.28084) / OPT
+
+            #Mobilisation time
+            IDS.at[index,'PYMoblisationDays'] = mobilisationDays
+               
+    return IDS
+
+def calculateWellStats(project='ADMA SARB Island UAE',rig='ND-78',well='SR54'):
+
+    """
+      Calculate the wellstats for a given well. This is to allow for the generation of
+      ft / day calcs
+
+    """
+
+    well = dataset.query('ProjectName=="'+project+'" & WellName=="'+well+'" & RigName=="'+rig+'" ')
+
+    std = well['StartDate'].dropna(axis=0,how='any')
+    endd = well['EndDate'].dropna(axis=0,how='any')
+    td = well['TopDepth'].dropna(axis=0,how='any')
+    bd = well['BottomDepth'].dropna(axis=0,how='any')
+
+    #if any entries come back NAN throw it in the f**king bin - we cannot calcualte on them anyway
+    if std.count() == 0 or endd.count() == 0 or td.count() == 0 or bd.count() == 0 :
+        return None
+
+
+    wellStat = {
+        'maxEndDT':endd.max(),
+        'minStartDT':std.min(),
+        'minTopDepth':td.min(),
+        'maxBottomDepth':bd.max()
+    }
+    
+
+    TDRecords = well.query('BottomDepth == '+str(wellStat['maxBottomDepth']))
+    TDRecords['EndDate'].dropna(axis=0,how='any',inplace=True)
+    wellStat['TDorCDreachedDT'] = TDRecords['EndDate'].min()
+  
+    print(wellStat)
+    return(wellStat)
+
+def calculateNPTStats(project='ADMA SARB Island UAE',rig='ND-78',well='SR54'):
+    """
+        Return the sum of Non productive time days
+
+    """
+
+    #Get NPT rows
+    npt = dataset.query('ProjectName=="'+project+'" & WellName=="'+well+'" & RigName=="'+rig+'" & TimeClassification=="Non Productive" ')
+    nptDays = npt['Duration(Days)'].sum(skipna = True)
+    return nptDays 
+
+def calculateFlatTime(project='ADMA SARB Island UAE',rig='ND-78',well='SR54',tddate=pd.Timestamp('2018-12-17 17:30:00.000')):
+    """
+        Sum the time for each entry where there has not been a change in
+        bottomdepth
+
+        && PowerBI_wdm_Operational_TimeBreakdown[EndDate]<=PowerBI_wdm_Operational_TimeBreakdown[15 WelTD or ft/day Day TBD])
+
+    """
+
+    flatTimeEntries = dataset.query('ProjectName=="'+project+'" & WellName=="'+well+'" & RigName=="'+rig+'" & TopDepth==BottomDepth & EndDate < "'+tddate+'" ')
+    flatTimeDays = flatTimeEntries['Duration(Days)'].sum(skipna = True)
+    return flatTimeDays
+
+def calculateMobilisation(project='ADMA SARB Island UAE',rig='ND-78',well='SR54'):
+
+   mobSingleWell = mob.query('Project_Name=="'+project+'" & Well=="'+well+'" & Rig=="'+rig+'" ')
+   mobDays = mobSingleWell['Duration(Days)'].sum(skipna = True)
+   return mobDays
+
+
+testDS()
+
+IDS = defineProjects()
+IDS.to_excel(r'IDS.xlsx', index = None, header=True)
